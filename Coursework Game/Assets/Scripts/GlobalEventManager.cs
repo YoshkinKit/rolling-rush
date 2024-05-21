f236e37911371fb0c976dd2b7f50e9d9450790bf
Jumaisimba <nikita.zhmak8872@gmail.com>
2024-05-21 21:19:40 +0300

    Added ui prefabs. Added changing panels in the main menu

--- Coursework Game/Assets/Scripts/GlobalEventManager.cs
+++ Coursework Game/Assets/Scripts/GlobalEventManager.cs
@@ -1,11 +1,9 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
 
 public class GlobalEventManager : MonoBehaviour
 {
-    public static UnityEvent OnFinish = new UnityEvent();
+    public static UnityEvent OnFinish = new();
 
     public static void SendOnFinish()
     {
---
+++ Coursework Game/Assets/Scripts/PanelManager.cs
@@ -0,0 +1,65 @@
+using UnityEngine;
+
+public class PanelManager : MonoBehaviour
+{
+    [SerializeField] private GameObject mainMenuPanel;
+    [SerializeField] private GameObject settingsPanel;
+    [SerializeField] private GameObject storePanel;
+    [SerializeField] private GameObject levelSelectPanel;
+
+    private GameObject _currentPanel;
+    private PanelState _currentState;
+
+    private void Start()
+    {
+        _currentState = PanelState.MainMenu;
+        _currentPanel = mainMenuPanel;
+        _currentPanel.SetActive(true);
+    }
+
+    private void ChangeState(PanelState newState)
+    {
+        if (_currentState == newState) return;
+        _currentPanel.SetActive(false);
+        _currentState = newState;
+
+        _currentPanel = _currentState switch
+        {
+            PanelState.MainMenu => mainMenuPanel,
+            PanelState.Settings => settingsPanel,
+            PanelState.Store => storePanel,
+            PanelState.LevelSelect => levelSelectPanel,
+            _ => mainMenuPanel
+        };
+
+        _currentPanel.SetActive(true);
+    }
+
+    public void ChangeToMainMenuPanel()
+    {
+        ChangeState(PanelState.MainMenu);
+    }
+    
+    public void ChangeToSettingsPanel()
+    {
+        ChangeState(PanelState.Settings);
+    }
+    
+    public void ChangeToStorePanel()
+    {
+        ChangeState(PanelState.Store);
+    }
+    
+    public void ChangeToLevelSelectPanel()
+    {
+        ChangeState(PanelState.LevelSelect);
+    }
+
+    private enum PanelState
+    {
+        MainMenu,
+        Settings,
+        Store,
+        LevelSelect
+    }
+}
