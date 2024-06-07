7881b1dc3d865fbad202da7e849b58092039565c
YoshkinKit <nikita.zhmak8872@gmail.com>
2024-06-07 21:03:39 +0300

    Started doing the audio system

---
+++ Coursework Game/Assets/Scripts/Audio/AudioManager.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace CourseworkGame.Audio
+{
+    public class AudioManager : MonoBehaviour
+    {
+        [SerializeField] private AudioClip uiClickSound;
+        
+        private AudioSource _audioSource;
+
+        private void Start()
+        {
+            _audioSource = GetComponent<AudioSource>();
+        }
+
+        public void PlayUIButtonClip()
+        {
+            _audioSource.PlayOneShot(uiClickSound);
+        }
+    }
+}
