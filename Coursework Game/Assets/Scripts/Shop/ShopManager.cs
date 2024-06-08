9c878f729e166a52ffd38083f03116cdc8185897
YoshkinKit <nikita.zhmak8872@gmail.com>
2024-06-08 13:16:28 +0300

    Started doing the player skin system

---
+++ Coursework Game/Assets/Scripts/Shop/ShopManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace CourseworkGame.Shop
+{
+    public class ShopManager : MonoBehaviour
+    {
+        [SerializeField] private List<ShopItemSO> shopItemsInfo;
+        [SerializeField] private List<ShopItemTemplate> shopItems;
+
+        private void Start()
+        {
+            var sprites = new List<Sprite>(Resources.LoadAll<Sprite>($"SkinSprites/"));
+            for (int i = 0; i < shopItemsInfo.Count; i++)
+            {
+                shopItems[i].enabled = true;
+                Instantiate(sprites.Find(s => s.name == shopItemsInfo[i].SkinName), shopItems[i].SkinSpriteSpawnPoint);
+                shopItems[i].SkinName.text = shopItemsInfo[i].SkinName;
+                shopItems[i].Price.text =
+                    shopItemsInfo[i].Price + (shopItemsInfo[i].Currency == ShopItemSO.CurrencyType.Coin
+                        ? "\nCoins"
+                        : "\nStars");
+            }
+        }
+    }
+}
